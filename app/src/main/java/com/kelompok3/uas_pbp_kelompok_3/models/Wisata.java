3ecc0413a94b1c59176ed186353679840c09080f
GFigo <190710060@students.uajy.ac.id>
2021-12-04 17:35:58 +0700

    first commit

---
+++ app/src/main/java/com/kelompok3/uas_pbp_kelompok_3/models/Wisata.java
@@ -0,0 +1,20 @@
+package com.kelompok3.uas_pbp_kelompok_3.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Wisata {
+    @SerializedName("nama_wisata")
+    private String nama_wisata;
+
+    @SerializedName("nama_wisata")
+    private String lokasi;
+
+    @SerializedName("nama_wisata")
+    private String deskripsi;
+
+    @SerializedName("nama_wisata")
+    private String url_gambar;
+
+    @SerializedName("nama_wisata")
+    private int harga;
+}
