637e627f60b7a4fca4439a2aa6338a9bf4d5f816
Asus <yohannes123@gmail.com>
2021-12-04 17:58:15 +0700

    RentalResponse

---
+++ app/src/main/java/com/kelompok3/uas_pbp_kelompok_3/models/RentalResponse.java
@@ -0,0 +1,28 @@
+package com.kelompok3.uas_pbp_kelompok_3.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RentalResponse {
+    private String message;
+
+    @SerializedName("rental")
+    private List<Rental> rentalList;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Rental> getRentalList() {
+        return rentalList;
+    }
+
+    public void setRentalList(List<Rental> rentalList) {
+        this.rentalList = rentalList;
+    }
+}
