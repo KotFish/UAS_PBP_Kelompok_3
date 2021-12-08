0c81bee3d46b8f34cc64b8438f9cea64c3943ac8
GFigo <190710060@students.uajy.ac.id>
2021-12-08 21:52:45 +0700

    bool fix & adding noplat, etc

---
+++ app/src/main/java/com/kelompok3/uas_pbp_kelompok_3/AddEditWisataActivity.java
@@ -0,0 +1,14 @@
+package com.kelompok3.uas_pbp_kelompok_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEditWisataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_wisata);
+    }
+}
