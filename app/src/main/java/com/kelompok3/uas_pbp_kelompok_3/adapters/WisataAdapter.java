17ed53060cbb5d53459b0a88e14b7bc196d5f8ad
Pandu Widiatmika <190710138@students.uajy.ac.id>
2021-12-09 01:15:12 +0800

    benerin RentalAdapter.java sama WisataAdapter.java

--- app/src/main/java/com/kelompok3/uas_pbp_kelompok_3/adapters/WisataAdapter.java
+++ app/src/main/java/com/kelompok3/uas_pbp_kelompok_3/adapters/WisataAdapter.java
@@ -18,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.gson.annotations.SerializedName;
 import com.kelompok3.uas_pbp_kelompok_3.AddEditRentalActivity;
+import com.kelompok3.uas_pbp_kelompok_3.AddEditWisataActivity;
 import com.kelompok3.uas_pbp_kelompok_3.MainActivity;
 import com.kelompok3.uas_pbp_kelompok_3.R;
 import com.kelompok3.uas_pbp_kelompok_3.WisataActivity;
@@ -40,24 +41,9 @@ public class WisataAdapter extends RecyclerView.Adapter<WisataAdapter.ViewHolder
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.item_rental, parent, false);
+        View view = inflater.inflate(R.layout.item_wisata, parent, false);
         return new ViewHolder(view);
     }
-
-    @SerializedName("nama_wisata")
-    private String nama_wisata;
-
-    @SerializedName("lokasi")
-    private String lokasi;
-
-    @SerializedName("deskripsi")
-    private String deskripsi;
-
-    @SerializedName("url_gambar")
-    private String url_gambar;
-
-    @SerializedName("harga")
-    private int harga;
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
@@ -79,7 +65,7 @@ public class WisataAdapter extends RecyclerView.Adapter<WisataAdapter.ViewHolder
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int
                                             i) {
-                                        if (context instanceof MainActivity)
+                                        if (context instanceof WisataActivity)
                                             ((WisataActivity)
                                                     context).deleteWisata(wisata.getId());
                                     }
@@ -90,11 +76,11 @@ public class WisataAdapter extends RecyclerView.Adapter<WisataAdapter.ViewHolder
         holder.cvWisata.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(context, AddEditRentalActivity.class);
+                Intent i = new Intent(context, AddEditWisataActivity.class);
                 i.putExtra("id", wisata.getId());
-                if (context instanceof MainActivity)
-                    ((MainActivity) context).startActivityForResult(i,
-                            MainActivity.LAUNCH_ADD_ACTIVITY);
+                if (context instanceof WisataActivity)
+                    ((WisataActivity) context).startActivityForResult(i,
+                            WisataActivity.LAUNCH_ADD_ACTIVITY);
             }
         });
     }
@@ -102,7 +88,7 @@ public class WisataAdapter extends RecyclerView.Adapter<WisataAdapter.ViewHolder
     public int getItemCount() {
         return filteredWisataList.size();
     }
-    public void setRentalList(List<Wisata> wisataList) {
+    public void setWisataList(List<Wisata> wisataList) {
         this.wisataList = wisataList;
         filteredWisataList = new ArrayList<>(wisataList);
     }
